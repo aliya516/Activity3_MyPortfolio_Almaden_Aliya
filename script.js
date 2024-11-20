cc77d4db2542d9f96b09cba198ccda871d3e7fe0
Aliya Beyonce Almaden <aliyabeyonce.almaden@tup.edu.ph>
2024-11-21 06:05:34 +0800

    Summary

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-/* Set the width of the sidebar to 250px (show it) */
-/*function openNav() {
-    document.getElementById("mySidepanel").style.width = "230px";
-  }*/
-
-  /*  function openNav() {
-        const panel = document.getElementById("mySidepanel");
-        panel.style.width = "230px";
-        panel.style.height = "1000px"; // Adjusted property
-    }*/
-  
-/*function openNav(){
-    document.getElementById("mySidepanel").style.length = "100px";
-}*/
-  /* Set the width of the sidebar to 0 (hide it) */
-  /*function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }*/
-    function openNav() {
+   function openNav() {
         document.getElementById("mySidepanel").style.width = "250px";
         document.querySelector(".openbtn").style.visibility = "hidden";
         document.querySelector(".closebtn").style.visibility = "visible";
